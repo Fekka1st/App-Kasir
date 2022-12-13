72549ea4a650cca9f2198a5c2daa93fbcaae464c
Ferry Aditya H <ferryaditya124@gmail.com>
2022-12-13 22:48:55 +0700

    #Modifikasi AdminLTE

---
+++ Kasir-app/resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2022 <a href="#">Kelompok G</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 0.1 Alpha
+    </div>
+</footer>
