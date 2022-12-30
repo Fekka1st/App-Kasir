ceb55c4d761e9ad5d0b1fa840debe5c831dd9124
Ferry Aditya H <ferryaditya124@gmail.com>
2022-12-30 19:06:02 +0700

    #Membuat Authentication menggunakan laravel Jetstream

--- Kasir-app/routes/web.php
+++ Kasir-app/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', fn () => redirect()->route('login'));
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('welcome');
+    })->name('dashboard');
 });
