ac0c944c5e4271c888f4b0fab0bab3b4008b2c68
Ferry Aditya H <ferryaditya124@gmail.com>
2023-01-06 08:12:18 +0700

    #membuat seeding users agar bisa login (kemarin pakai tinker untuk isi table users)

---
+++ Kasir-app/database/seeders/Users.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class Users extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('12345'),
+                'level' => '1'
+            ]
+        ];
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
+    }
+}
