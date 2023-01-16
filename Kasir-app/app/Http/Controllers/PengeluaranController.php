fab1e854273bb62e0a81c61c30b30e7fb9ef181c
Ferry Aditya H <ferryaditya124@gmail.com>
2023-01-16 08:12:12 +0700

    #CRUD pengeluaran selesai

--- Kasir-app/app/Http/Controllers/PengeluaranController.php
+++ Kasir-app/app/Http/Controllers/PengeluaranController.php
@@ -12,4 +12,59 @@ class PengeluaranController extends Controller
     {
         return view('pengeluaran.index');
     }
+
+    public function data()
+    {
+        $pengeluaran = Pengeluaran::orderBy('id_pengeluaran', 'desc')->get();
+
+        return datatables()
+            ->of($pengeluaran)
+            ->addIndexColumn()
+            ->addColumn('created_at', function ($pengeluaran) {
+                return format_date($pengeluaran->created_at, false);
+            })
+            ->addColumn('nominal', function ($pengeluaran) {
+                return format_uang($pengeluaran->nominal);
+            })
+            ->addColumn('aksi', function ($pengeluaran) {
+                return '
+                <div class="btn-group">
+                    <button type="button" onclick="edit(`' . route('pengeluaran.update', $pengeluaran->id_pengeluaran) . '`)" class="btn btn-xs btn-info btn-flat">Edit</button>
+                    <button type="button" onclick="hapus(`' . route('pengeluaran.destroy', $pengeluaran->id_pengeluaran) . '`)" class="btn btn-xs btn-danger btn-flat">Hapus</button>
+                </div>
+                ';
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+
+    public function show($id)
+    {
+        $pengeluaran = Pengeluaran::find($id);
+
+        return response()->json($pengeluaran);
+    }
+
+    public function store(Request $request)
+    {
+        $pengeluaran = Pengeluaran::create($request->all());
+
+        return response()->json('Data berhasil disimpan', 200);
+    }
+
+
+
+    public function update(Request $request, $id)
+    {
+        $pengeluaran = Pengeluaran::find($id)->update($request->all());
+
+        return response()->json('Data berhasil disimpan', 200);
+    }
+
+    public function destroy($id)
+    {
+        $pengeluaran = Pengeluaran::find($id)->delete();
+
+        return response(null, 204);
+    }
 }
--- Kasir-app/resources/views/produk/index.blade.php
+++ Kasir-app/resources/views/produk/index.blade.php
@@ -165,10 +165,7 @@
         }
 
         function hapus(url) {
-            // if (confirm('Ingin hapus data ?')) {
-            //    
-            // }
-
+        
             swal({
                     title: "Apakah Kamu Yakin",
                     text: "Data terhapus tidak dapat kembali lagi",
