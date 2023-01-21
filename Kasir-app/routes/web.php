c2ed11c24ee4aa18475a00fd380934abc34d98c7
Ferry Aditya H <ferryaditya124@gmail.com>
2023-01-21 13:59:14 +0700

    #middleware route beres

--- Kasir-app/routes/web.php
+++ Kasir-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\PengeluaranController;
@@ -12,7 +13,7 @@ use App\Http\Controllers\PenjualanDetailController;
 use App\Http\Controllers\SendEmail;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -47,61 +48,87 @@ Route::middleware([
 });
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/kategori/data', [KategoriController::class, 'data'])->name('kategori.data');
-    Route::resource('/kategori', KategoriController::class); // ini bukan array guys hati hati yah
 
-    Route::get('/supplier/data', [SupplierController::class, 'data'])->name('supplier.data');
-    Route::resource('/supplier', SupplierController::class);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('/produk/data', [ProdukController::class, 'data'])->name('produk.data');
-    Route::post('/produk/delete-selected', [ProdukController::class, 'deleteselected'])->name('produk.delete_selected');
-    Route::post('/produk/cetak-barcode', [ProdukController::class, 'cetakbarcode'])->name('produk.cetak_barcode');
-    Route::resource('/produk', ProdukController::class);
+    route::group(['middleware' => '1'], function () {
+        Route::get('/kategori/data', [KategoriController::class, 'data'])->name('kategori.data');
+        Route::resource('/kategori', KategoriController::class); // ini bukan array guys hati hati yah
 
+        Route::get('/supplier/data', [SupplierController::class, 'data'])->name('supplier.data');
+        Route::resource('/supplier', SupplierController::class);
 
-    Route::get('/member/data', [MemberController::class, 'data'])->name('member.data');
-    Route::resource('/member', MemberController::class);
-    Route::post('/member/cetak-member', [MemberController::class, 'cetakmember'])->name('member.cetak_member');
-
-    Route::get('/pengeluaran/data', [PengeluaranController::class, 'data'])->name('pengeluaran.data');
-    Route::resource('/pengeluaran', PengeluaranController::class);
-    // mailer
-    Route::get('/send-email', [SendEmail::class, 'index'])->name('permintaan.barang');
-
-    Route::get('/pembelian/data', [PembelianController::class, 'data'])->name('pembelian.data');
-    Route::get('/pembelian/{id}/create', [PembelianController::class, 'create'])->name('pembelian.create');
-    Route::resource('/pembelian', PembelianController::class)
-        ->except('create');
-
-    Route::get('/pembelian_detail/{id}/data', [PembelianDetailController::class, 'data'])->name('pembelian_detail.data');
-    Route::get('/pembelian_detail/loadform/{diskon}/{total}', [PembelianDetailController::class, 'loadForm'])->name('pembelian_detail.load_form');
-    Route::resource('/pembelian_detail', PembelianDetailController::class)
-        ->except('create', 'show', 'edit');
+        Route::get('/produk/data', [ProdukController::class, 'data'])->name('produk.data');
+        Route::post('/produk/delete-selected', [ProdukController::class, 'deleteselected'])->name('produk.delete_selected');
+        Route::post('/produk/cetak-barcode', [ProdukController::class, 'cetakbarcode'])->name('produk.cetak_barcode');
+        Route::resource('/produk', ProdukController::class);
 
 
-    Route::get('/transaksi/baru', [PenjualanController::class, 'create'])->name('transaksi.baru');
-    Route::post('/transaksi/simpan', [PenjualanController::class, 'store'])->name('transaksi.simpan');
-    Route::get('/transaksi/selesai', [PenjualanController::class, 'selesai'])->name('transaksi.selesai');
-    Route::get('/transaksi/nota-kecil', [PenjualanController::class, 'notaKecil'])->name('transaksi.nota_kecil');
-    Route::get('/transaksi/nota-besar', [PenjualanController::class, 'notaBesar'])->name('transaksi.nota_besar');
+        Route::get('/member/data', [MemberController::class, 'data'])->name('member.data');
+        Route::resource('/member', MemberController::class);
+        Route::post('/member/cetak-member', [MemberController::class, 'cetakmember'])->name('member.cetak_member');
 
-    Route::get('/transaksi/{id}/data', [PenjualanDetailController::class, 'data'])->name('transaksi.data');
-    Route::get('/transaksi/loadform/{diskon}/{total}/{diterima}', [PenjualanDetailController::class, 'loadForm'])->name('transaksi.load_form');
-    Route::resource('/transaksi', PenjualanDetailController::class)
-        ->except('create', 'show', 'edit');
-    Route::post('/transaksi/tambah/', [PenjualanDetailController::class, 'tambah'])->name('tambah');
-    Route::post('/transaksi/bayar/', [PenjualanDetailController::class, 'bayar'])->name('pembayaran');
-    Route::post('/transaksi/simpan-transaski/', [PenjualanDetailController::class, 'simpanTransaksi'])->name('simpanTransaksi');
+        Route::get('/pengeluaran/data', [PengeluaranController::class, 'data'])->name('pengeluaran.data');
+        Route::resource('/pengeluaran', PengeluaranController::class);
+        // mailer
+        Route::get('/send-email', [SendEmail::class, 'index'])->name('permintaan.barang');
 
-    Route::get('/penjualan/data', [PenjualanController::class, 'data'])->name('penjualan.data');
-    Route::get('/penjualan', [PenjualanController::class, 'index'])->name('penjualan.index');
-    Route::get('/penjualan/{id}', [PenjualanController::class, 'show'])->name('penjualan.show');
-    Route::delete('/penjualan/{id}', [PenjualanController::class, 'destroy'])->name('penjualan.destroy');
+        Route::get('/pembelian/data', [PembelianController::class, 'data'])->name('pembelian.data');
+        Route::get('/pembelian/{id}/create', [PembelianController::class, 'create'])->name('pembelian.create');
+        Route::resource('/pembelian', PembelianController::class)
+            ->except('create');
 
-    Route::get('/print_report', [PengeluaranController::class, 'report'])->name('pengeluaran.cetak');
+        Route::get('/pembelian_detail/{id}/data', [PembelianDetailController::class, 'data'])->name('pembelian_detail.data');
+        Route::get('/pembelian_detail/loadform/{diskon}/{total}', [PembelianDetailController::class, 'loadForm'])->name('pembelian_detail.load_form');
+        Route::resource('/pembelian_detail', PembelianDetailController::class)
+            ->except('create', 'show', 'edit');
 
-    Route::get('/user/data', [UserController::class, 'data'])->name('user.data');
-    Route::resource('/user', UserController::class);
+
+        Route::get('/transaksi/baru', [PenjualanController::class, 'create'])->name('transaksi.baru');
+        Route::post('/transaksi/simpan', [PenjualanController::class, 'store'])->name('transaksi.simpan');
+        Route::get('/transaksi/selesai', [PenjualanController::class, 'selesai'])->name('transaksi.selesai');
+        Route::get('/transaksi/nota-kecil', [PenjualanController::class, 'notaKecil'])->name('transaksi.nota_kecil');
+        Route::get('/transaksi/nota-besar', [PenjualanController::class, 'notaBesar'])->name('transaksi.nota_besar');
+
+        Route::get('/transaksi/{id}/data', [PenjualanDetailController::class, 'data'])->name('transaksi.data');
+        Route::get('/transaksi/loadform/{diskon}/{total}/{diterima}', [PenjualanDetailController::class, 'loadForm'])->name('transaksi.load_form');
+        Route::resource('/transaksi', PenjualanDetailController::class)
+            ->except('create', 'show', 'edit');
+        Route::post('/transaksi/tambah/', [PenjualanDetailController::class, 'tambah'])->name('tambah');
+        Route::post('/transaksi/bayar/', [PenjualanDetailController::class, 'bayar'])->name('pembayaran');
+        Route::post('/transaksi/simpan-transaski/', [PenjualanDetailController::class, 'simpanTransaksi'])->name('simpanTransaksi');
+
+        Route::get('/penjualan/data', [PenjualanController::class, 'data'])->name('penjualan.data');
+        Route::get('/penjualan', [PenjualanController::class, 'index'])->name('penjualan.index');
+        Route::get('/penjualan/{id}', [PenjualanController::class, 'show'])->name('penjualan.show');
+        Route::delete('/penjualan/{id}', [PenjualanController::class, 'destroy'])->name('penjualan.destroy');
+
+        Route::get('/print_report', [PengeluaranController::class, 'report'])->name('pengeluaran.cetak');
+
+        Route::get('/user/data', [UserController::class, 'data'])->name('user.data');
+        Route::resource('/user', UserController::class);
+    });
+
+    route::group(['middleware' => '1', '2'], function () {
+        Route::get('/transaksi/baru', [PenjualanController::class, 'create'])->name('transaksi.baru');
+        Route::post('/transaksi/simpan', [PenjualanController::class, 'store'])->name('transaksi.simpan');
+        Route::get('/transaksi/selesai', [PenjualanController::class, 'selesai'])->name('transaksi.selesai');
+        Route::get('/transaksi/nota-kecil', [PenjualanController::class, 'notaKecil'])->name('transaksi.nota_kecil');
+        Route::get('/transaksi/nota-besar', [PenjualanController::class, 'notaBesar'])->name('transaksi.nota_besar');
+
+        Route::get('/transaksi/{id}/data', [PenjualanDetailController::class, 'data'])->name('transaksi.data');
+        Route::get('/transaksi/loadform/{diskon}/{total}/{diterima}', [PenjualanDetailController::class, 'loadForm'])->name('transaksi.load_form');
+        Route::resource('/transaksi', PenjualanDetailController::class)
+            ->except('create', 'show', 'edit');
+        Route::post('/transaksi/tambah/', [PenjualanDetailController::class, 'tambah'])->name('tambah');
+        Route::post('/transaksi/bayar/', [PenjualanDetailController::class, 'bayar'])->name('pembayaran');
+        Route::post('/transaksi/simpan-transaski/', [PenjualanDetailController::class, 'simpanTransaksi'])->name('simpanTransaksi');
+
+        Route::get('/penjualan/data', [PenjualanController::class, 'data'])->name('penjualan.data');
+        Route::get('/penjualan', [PenjualanController::class, 'index'])->name('penjualan.index');
+        Route::get('/penjualan/{id}', [PenjualanController::class, 'show'])->name('penjualan.show');
+        Route::delete('/penjualan/{id}', [PenjualanController::class, 'destroy'])->name('penjualan.destroy');
+    });
 });
 
 // Route::get('/send-email', [SendEmail::class, 'index'])->name('permintaan.barang');
